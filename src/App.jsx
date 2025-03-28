99fb88deea12520966d486c2f65584a0a6f9dd9b
Ramit Vishwakarma <vishwakarmaramit@gmail.com>
2025-03-28 12:01:47 +0530

    Added vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import HomePage from "./Components/Page/HomePage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <HomePage />
     </>
   );
